9932ae690606ab21db9f7226f24de77c584afcb3
Corey Mckeel <cmckeel@codingdojo.com>
2023-01-19 02:12:09 -0500

    removed candyShop

--- FullStackMERN/candyShop/server/config/mongoose.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-    .connect("mongodb://127.0.0.1:27017/candyshop", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("Connected to database"))
-    .catch((err) => console.log("Failed to connect to mongoDB", err));
--- FullStackMERN/candyShop/server/routes/candy.routes.js
+++
@@ -1,9 +0,0 @@
-const CandyController = require("../controllers/candy.controllers");
-
-module.exports = (app) => {
-    app.get("/api/candies", CandyController.findAll);
-    app.get("/api/candies/:id", CandyController.findOne);
-    app.post("/api/candies", CandyController.create);
-    app.put("/api/candies/:id", CandyController.update);
-    app.delete("/api/candies/:id", CandyController.delete)
-};
